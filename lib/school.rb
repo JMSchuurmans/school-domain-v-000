35e0726ff5d1d913aec454a7b56c2609f8a7528c
Jeremy Schuurmans <jeremyschuurmans@gmail.com>
2018-05-25 19:28:09 +0000

    automatically backed up by learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,24 @@
-# code here!
+class School
+
+  attr_reader :name, :roster
+
+  def initialize(name)
+    @name = name
+    @roster = {}
+  end
+
+  def add_student(name, grade)
+    if @roster.include?(grade)
+      @roster[grade] << name
+    else
+      @roster[grade] = [name]
+    end
+
+    self.roster[grade] ||= []
+    self.roster[grade] << name
+  end
+
+  
+
+
+end
